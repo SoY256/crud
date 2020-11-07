1df96179e933b201ca614b92c97aade14a0c1445
SoY256 <70964574+SoY256@users.noreply.github.com>
2020-11-07 08:05:43 +0100

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "123",
-  DB: "testdb",
+  HOST: "ec2-54-217-236-206.eu-west-1.compute.amazonaws.com",
+  USER: "qsirdwbxoupawp",
+  PASSWORD: "1454ad61a527b2fef2c968b0ef6fb91ff0c32bfc783eb2f7579786fd9e617d1b",
+  DB: "d6qceqe8euehkv",
   dialect: "postgres",
   pool: {
     max: 5,
